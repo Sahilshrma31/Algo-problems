0f11a3b4b561805abe5f888aa4eaaac3717b1f68
Sahil.Sharma3184@gmail.com <sahil.sharma3184@gmail.com>
2025-08-20 01:26:17 +0530

    graphs representation in java

---
+++  GraphRepresentation1.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+class GraphRepresentation1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // take n and m
+        int n = sc.nextInt(); // number of vertices
+        int m = sc.nextInt(); // number of edges
+
+        // adjacency matrix (1-indexed, so use n+1)
+        int[][] adjMatrix = new int[n + 1][m + 1];
+
+        // take m edges
+        for (int i = 0; i < m; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+
+            adjMatrix[u][v] = 1;
+            adjMatrix[v][u] = 1; // because undirected
+        }
+
+        // print adjacency matrix
+        System.out.println("Adjacency Matrix:");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print(adjMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
+
+//input pre defined 
+class GraphRepresentation {
+    public static void main(String[] args) {
+        int n = 5; // number of vertices
+        int m = 6; // number of edges
+
+        // adjacency matrix (1-indexed, so use n+1)
+        int[][] adjMatrix = new int[n + 1][n + 1];
+
+        // predefined edges (u, v)
+        int[][] edges = {
+            {1, 2},
+            {1, 5},
+            {2, 3},
+            {2, 4},
+            {3, 4},
+            {4, 5}
+        };
+
+        // build the adjacency matrix
+        for (int i = 0; i < m; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+
+            adjMatrix[u][v] = 1;
+            adjMatrix[v][u] = 1; // undirected
+        }
+
+        // print adjacency matrix
+        System.out.println("Adjacency Matrix:");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print(adjMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
---
+++ GraphRepresentation2.java
@@ -0,0 +1,103 @@
+import java.util.*;
+
+class GraphRepresentation {
+    // Pair class to store (node, weight) for weighted graphs
+    static class Pair {
+        int node, weight;
+        Pair(int node, int weight) {
+            this.node = node;
+            this.weight = weight;
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5; // number of vertices
+        int m = 6; // number of edges
+
+        // predefined edges (u, v)
+        int[][] edges = {
+            {1, 2},
+            {1, 5},
+            {2, 3},
+            {2, 4},
+            {3, 4},
+            {4, 5}
+        };
+
+        // predefined weighted edges (u, v, w)
+        int[][] weightedEdges = {
+            {1, 2, 4},
+            {1, 5, 2},
+            {2, 3, 7},
+            {2, 4, 1},
+            {3, 4, 3},
+            {4, 5, 5}
+        };
+
+        // ---------------- Unweighted Undirected ----------------
+        ArrayList<ArrayList<Integer>> undirectedAdj = new ArrayList<>();
+        for (int i = 0; i <= n; i++) undirectedAdj.add(new ArrayList<>());
+
+        for (int i = 0; i < m; i++) {
+            int u = edges[i][0], v = edges[i][1];
+            undirectedAdj.get(u).add(v);
+            undirectedAdj.get(v).add(u); // both sides
+        }
+
+        System.out.println("Adjacency List for Unweighted Undirected Graph:");
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + " -> ");
+            for (int node : undirectedAdj.get(i)) System.out.print(node + " ");
+            System.out.println();
+        }
+
+        // ---------------- Unweighted Directed ----------------
+        ArrayList<ArrayList<Integer>> directedAdj = new ArrayList<>();
+        for (int i = 0; i <= n; i++) directedAdj.add(new ArrayList<>());
+
+        for (int i = 0; i < m; i++) {
+            int u = edges[i][0], v = edges[i][1];
+            directedAdj.get(u).add(v); // only one direction
+        }
+
+        System.out.println("\nAdjacency List for Unweighted Directed Graph:");
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + " -> ");
+            for (int node : directedAdj.get(i)) System.out.print(node + " ");
+            System.out.println();
+        }
+
+        // ---------------- Weighted Undirected ----------------
+        ArrayList<ArrayList<Pair>> weightedUndirectedAdj = new ArrayList<>();
+        for (int i = 0; i <= n; i++) weightedUndirectedAdj.add(new ArrayList<>());
+
+        for (int i = 0; i < m; i++) {
+            int u = weightedEdges[i][0], v = weightedEdges[i][1], w = weightedEdges[i][2];
+            weightedUndirectedAdj.get(u).add(new Pair(v, w));
+            weightedUndirectedAdj.get(v).add(new Pair(u, w)); // both sides
+        }
+
+        System.out.println("\nAdjacency List for Weighted Undirected Graph:");
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + " -> ");
+            for (Pair p : weightedUndirectedAdj.get(i)) System.out.print("(" + p.node + "," + p.weight + ") ");
+            System.out.println();
+        }
+
+        // ---------------- Weighted Directed ----------------
+        ArrayList<ArrayList<Pair>> weightedDirectedAdj = new ArrayList<>();
+        for (int i = 0; i <= n; i++) weightedDirectedAdj.add(new ArrayList<>());
+
+        for (int i = 0; i < m; i++) {
+            int u = weightedEdges[i][0], v = weightedEdges[i][1], w = weightedEdges[i][2];
+            weightedDirectedAdj.get(u).add(new Pair(v, w)); // only one direction
+        }
+
+        System.out.println("\nAdjacency List for Weighted Directed Graph:");
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + " -> ");
+            for (Pair p : weightedDirectedAdj.get(i)) System.out.print("(" + p.node + "," + p.weight + ") ");
+            System.out.println();
+        }
+    }
+}
